ac506673bd78637084cbe7a1c7574914cbc3e1f1
Anthony <anthonysiu@gmail.com>
2016-09-07 02:55:36 -0700

    order basic

--- app/src/main/java/com/example/siuant/myapplication/MainActivity.java
+++ app/src/main/java/com/example/siuant/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.siuant.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -12,31 +14,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_java);
 
-        /*TextView textView = new TextView(this);
-        String msg = getResources().getString(R.string.birthday);
-        String person = getResources().getString(R.string.person_name);
-        String message = msg + person + "!";
-        textView.setText(message);
-
-        //layoutParams = new RelativeLayout.LayoutParams(LayoutParams.)
-
-
-        ViewGroup layout = (ViewGroup) findViewById(R.id.main_layout);
-
-        layout.addView(textView);*/
-
-        /*String[] textArray = {"One", "Two", "Three", "Four"};
-        LinearLayout linearLayout = new LinearLayout(this);
-        setContentView(linearLayout);
-        linearLayout.setOrientation(LinearLayout.VERTICAL);
-        for( int i = 0; i < textArray.length; i++ )
-        {
-            TextView textView = new TextView(this);
-            textView.setText(textArray[i]);
-            linearLayout.addView(textView);
-        }*/
 
     }
+    public void submitOrder(View view){
+        display(1);
+    }
+
+    public void display(int num){
+        TextView quantityTextView = (TextView)findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + num);
+    }
 }
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }*/
