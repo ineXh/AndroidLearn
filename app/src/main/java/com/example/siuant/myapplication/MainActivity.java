809cf4c9ccf3b79cb7d0de0befc92b79be6cd138
Anthony <anthonysiu@gmail.com>
2016-09-07 10:59:48 -0700

    before rearrange layout

--- app/src/main/java/com/example/siuant/myapplication/MainActivity.java
+++ app/src/main/java/com/example/siuant/myapplication/MainActivity.java
@@ -9,24 +9,42 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.NumberFormat;
 
+public class MainActivity extends AppCompatActivity {
+    int quantity = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_java);
 
-
+        display(quantity);
     }
     public void submitOrder(View view){
-        display(1);
+        displayPrice(quantity*5);
     }
 
     public void display(int num){
         TextView quantityTextView = (TextView)findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + num);
     }
-}
+
+    public void displayPrice(int number){
+        TextView priceTextView = (TextView)findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    public void increment(View view){
+        quantity++;
+        display(quantity);
+    }
+
+    public void decrement(View view){
+        quantity--;
+        if(quantity < 0) quantity = 0;
+        display(quantity);
+    }
+} // end MainActivity
 /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
